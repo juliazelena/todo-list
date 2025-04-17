90cf9c32214878c308b11c506d5ad9b147f85813
ulichka <ulichka7615@gmail.com>
2025-04-17 15:43:19 +0200

    add path for tag list

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from catalog.views import index
+from catalog.views import index, TagListView
 
 
 urlpatterns = [
     path("", index, name="index"),
+    path("tags/", TagListView.as_view(), name="tag-list")
 ]
 
 app_name = "catalog"
