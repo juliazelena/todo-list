cef61e2c9d5f0166cd5d528e874319e3edef4ffe
ulichka <ulichka7615@gmail.com>
2025-04-17 17:19:53 +0200

    add path for deleting tags

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,12 +1,19 @@
 from django.urls import path
 
-from catalog.views import index, TagListView, TagCreateView, TagUpdateView
+from catalog.views import (
+    index,
+    TagListView,
+    TagCreateView,
+    TagUpdateView,
+    TagDeleteView,
+)
 
 urlpatterns = [
     path("", index, name="index"),
     path("tags/", TagListView.as_view(), name="tag-list"),
     path("tags/create/", TagCreateView.as_view(), name="tag-create"),
     path("tags/<int:pk>/update/", TagUpdateView.as_view(), name="tag-update"),
+    path("tags/<int:pk>/delete/", TagDeleteView.as_view(),name="tag-delete"),
 ]
 
 app_name = "catalog"
