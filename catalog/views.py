135a4a870317c0cb2cc8b8ebc2d3d9932a520a74
ulichka <ulichka7615@gmail.com>
2025-04-17 15:22:48 +0200

    create view for index url

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,17 @@
+from django.core.paginator import Paginator
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+from catalog.models import Post
+
+
+def index(request: HttpRequest) -> HttpResponse:
+    post_list = Post.objects.all()
+    paginator = Paginator(post_list, 3)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+    context = {
+        "post_list": post_list,
+        "page_obj": page_obj,
+    }
+    return render(request, "catalog/index.html", context=context)
