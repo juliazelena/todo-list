1d47374d018eccf5ccdd15da47fae9410bc39bd4
ulichka <ulichka7615@gmail.com>
2025-04-17 16:28:56 +0200

    create view for url tag-create

--- catalog/views.py
+++ catalog/views.py
@@ -1,6 +1,7 @@
 from django.core.paginator import Paginator
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
+from django.urls import reverse_lazy
 from django.views import generic
 
 from catalog.models import Post, Tag
@@ -21,3 +22,10 @@ def index(request: HttpRequest) -> HttpResponse:
 class TagListView(generic.ListView):
     model = Tag
     paginate_by = 5
+    
+
+class TagCreateView(generic.CreateView):
+    model = Tag
+    fields = "__all__"
+    success_url = reverse_lazy("catalog:tag-list")
+    template_name = "catalog/tag_form.html"
