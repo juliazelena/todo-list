ec28285258535bbd4a338c6c97f09627155884a4
ulichka <ulichka7615@gmail.com>
2025-04-17 16:43:35 +0200

    add path for updating tags

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
-from catalog.views import index, TagListView, TagCreateView
+from catalog.views import index, TagListView, TagCreateView, TagUpdateView
 
 urlpatterns = [
     path("", index, name="index"),
     path("tags/", TagListView.as_view(), name="tag-list"),
     path("tags/create/", TagCreateView.as_view(), name="tag-create"),
+    path("tags/<int:pk>/update/", TagUpdateView.as_view(), name="tag-update"),
 ]
 
 app_name = "catalog"
