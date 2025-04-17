821d235a1d3302f542fdaa5f3864b5eaf191bad9
ulichka <ulichka7615@gmail.com>
2025-04-17 16:27:36 +0200

    add path for creating new tag

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 
-from catalog.views import index, TagListView
-
+from catalog.views import index, TagListView, TagCreateView
 
 urlpatterns = [
     path("", index, name="index"),
-    path("tags/", TagListView.as_view(), name="tag-list")
+    path("tags/", TagListView.as_view(), name="tag-list"),
+    path("tags/create/", TagCreateView.as_view(), name="tag-create"),
 ]
 
 app_name = "catalog"
